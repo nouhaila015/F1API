ad07445ade6775c3a7e386a13e007fe1bbfdce53
Nouhaila <mghanen.n@sfeir.com>
2026-04-08 11:51:48 +0200

    configure WebClientConfig

---
+++ src/main/java/com/f1/api/config/WebClientConfig.java
@@ -0,0 +1,27 @@
+package com.f1.api.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.ClientCodecConfigurer;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Value("${openf1.base-url}")
+    private String f1ApiUrl;
+
+    @Bean
+    public WebClient webClient() {
+        ExchangeStrategies strategies = ExchangeStrategies.builder()
+                .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(10 * 1024 * 1024))
+                .build();
+
+        return WebClient.builder()
+                .baseUrl(f1ApiUrl)
+                .exchangeStrategies(strategies)
+                .build();
+    }
+}
