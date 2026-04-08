b8ebfc403a7cf2d2662b3301658bc5ea3de6acc7
Nouhaila <mghanen.n@sfeir.com>
2026-04-08 11:34:26 +0200

    Initialize project

---
+++ src/test/java/com/f1/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f1.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
